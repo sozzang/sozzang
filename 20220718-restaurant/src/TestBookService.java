691a15695fd219ebab16e954fc0f2b8308df7ae0
sozzang <104603767+sozzang@users.noreply.github.com>
2022-09-19 16:30:35 +0900

    Delete 20220718-restaurant directory

--- 20220718-restaurant/src/TestBookService.java
+++
@@ -1,7 +0,0 @@
-public class TestBookService {
-	public static void main(String[] args) {
-		BookService service = new BookService(new BookDaoParamConn());
-//		service.insertAndUpdate("책", 9999);
-		service.insertAndUpdate("중복이름", 654);
-	}
-}
